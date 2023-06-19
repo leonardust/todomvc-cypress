4820a658332c0173ad0c647678f2df454b03e0f6
Lukasz Komorowski <lukasz.komorowski.edu@gmail.com>
2023-06-19 12:35:28 +0200

    add custom data dynaically

--- cypress/multiple-cucumber-report-config.js
+++ cypress/multiple-cucumber-report-config.js
@@ -1,18 +1,38 @@
 const report = require("multiple-cucumber-html-reporter");
 
+const browserName = process.env.BROWSER_NAME || "unknown-browser";
+const browserVersion = process.env.BROWSER_VERSION || "unknown-version";
+const device = process.env.DEVICE || "unknown-device";
+const platformName = process.env.PLATFORM_NAME || "unknown-platform";
+const platformVersion = process.env.PLATFORM_VERSION || "unknown-version";
+
+const cypressVersion = require("cypress/package.json").version;
+const nodeVersion = process.version;
+
 report.generate({
   jsonDir: "./cypress/reports/cucumber-json/",
   reportPath: "./cypress/reports/cucumber-report/",
   openReportInBrowser: true,
   displayDuration: true,
   displayReportTime: true,
+  metadata: {
+    browser: {
+      name: browserName,
+      version: browserVersion,
+    },
+    device: device,
+    platform: {
+      name: platformName,
+      version: platformVersion,
+    },
+  },
   customData: {
     title: "Run info",
     data: [
       { label: "Project", value: "todomvc-cucumber" },
       { label: "Release", value: "1.0.0" },
-      { label: "Node Version", value: "16.16.0" },
-      { label: "Cypress Version", value: "12.14.0" },
+      { label: "Node Version", value: nodeVersion },
+      { label: "Cypress Version", value: cypressVersion },
     ],
   },
 });
