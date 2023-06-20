6ac6ef41b05eabc1b6360f0e0e4c2cd60890c0fe
Lukasz Komorowski <lukasz.komorowski.edu@gmail.com>
2023-06-20 17:10:41 +0200

    update summary, comment screenshots

--- cypress/support/step_definitions/common/common-assertions-steps.js
+++ cypress/support/step_definitions/common/common-assertions-steps.js
@@ -3,5 +3,5 @@ import { Then } from "@badeball/cypress-cucumber-preprocessor";
 Then("I see the home page", () => {
   cy.get(".header").should("be.visible");
   cy.get(".info").should("be.visible");
-  cy.screenshot("home-page");
+  // cy.screenshot("home-page");
 });
